03c269fab0ac87a55ff94974c41fc036ea521795
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-07 13:47:34 +0200

    A few changes

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PeopleDashboard = () => {
+    return (
+        <div>
+            <h1>People dashboard</h1>
+        </div>
+    )
+}
---
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SettingsDashboard = () => {
+    return (
+        <div>
+            <h1>Settings dashboard</h1>
+        </div>
+    )
+}

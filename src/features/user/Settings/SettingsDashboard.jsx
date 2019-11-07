8ca304985bee5474b0a910f1b9f56ebeb6e6d86d
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-07 15:34:58 +0200

    •internal navigation for our app •working on the settings page

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import SettingsNav from "./SettingsNav";
+import { Route, Redirect, Switch } from "react-router";
+import BasicPage from "./BasicPage";
+import PhotosPage from "./PhotosPage";
+import AccountPage from "./AccountPage";
+import AboutPage from "./AboutPage";
 
 export const SettingsDashboard = () => {
-    return (
-        <div>
-            <h1>Settings dashboard</h1>
-        </div>
-    )
-}
+  return (
+    <Grid>
+      <GridColumn width={12}>
+        <Switch>
+          <Redirect exact from='/settings' to='/settings/basic' />
+          <Route path='/settings/basic' component={BasicPage} />
+          <Route path='/settings/about' component={AboutPage} />
+          <Route path='/settings/photos' component={PhotosPage} />
+          <Route path='/settings/account' component={AccountPage} />
+        </Switch>
+      </GridColumn>
+      <GridColumn width={4}>
+        <SettingsNav />
+      </GridColumn>
+    </Grid>
+  );
+};
 
 export default SettingsDashboard;
--- src/features/user/Settings/SettingsNav.jsx
+++ src/features/user/Settings/SettingsNav.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Menu, Header, Grid } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 export const SettingsNav = () => {
-    return (
-        <div>
-            <h1>Settings Nav</h1>
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <Menu vertical>
+        <Header icon='user' attached inverted color='grey' content='Profile' />
+        <Menu.Item as={NavLink} to='/settings/basic'>
+          Basics
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/settings/about'>
+          About Me
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/settings/photos'>
+          My Photos
+        </Menu.Item>
+      </Menu>
+      <Grid.Row />
+      <Menu vertical>
+        <Header
+          icon='settings'
+          attached
+          inverted
+          color='grey'
+          content='Account'
+        />
+        <Menu.Item as={NavLink} to='/settings/account'>
+          My Account
+        </Menu.Item>
+      </Menu>
+    </Fragment>
+  );
+};
 
 export default SettingsNav;

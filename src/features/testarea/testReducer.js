7b736b7789538d5115c672929ed1c2fb1394b8ea
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-08 15:44:23 +0200

    Reducing boilerplate code

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,10 +1,24 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+import { createReducer } from "../../app/common/util/reducerUtils";
 
 const initialState = {
   data: 42
 };
 
-const testReducer = (state = initialState, action) => {
+const incrementCounter = state => {
+  return { ...state, data: state.data + 1 };
+};
+
+const decrementCounter = state => {
+  return { ...state, data: state.data - 1 };
+};
+
+export default createReducer(initialState, {
+  [INCREMENT_COUNTER]: incrementCounter,
+  [DECREMENT_COUNTER]: decrementCounter
+});
+
+/* const testReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT_COUNTER:
       return { ...state, data: state.data + 1 };
@@ -13,6 +27,4 @@ const testReducer = (state = initialState, action) => {
     default:
       return state;
   }
-};
-
-export default testReducer;
+};*/

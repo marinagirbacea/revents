2f2bfc04f5c08ae2866d0acd74992358a7171fb9
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-11 16:55:55 +0200

    Connecting the event detailed page to the store and Fixing the scroll issue on routing to a new page

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
-const store=configureStore();
+const store = configureStore();
 
 console.log(store.getState());
 
@@ -17,7 +18,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl

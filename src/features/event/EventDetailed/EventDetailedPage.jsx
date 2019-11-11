17021a872675fd2cb061bae52945f9439de0c087
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-11 11:29:41 +0200

    Creating the event detailed page structure

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Grid, GridColumn } from 'semantic-ui-react'
+import EventDetailedHeader from './EventDetailedHeader'
+import EventDetailedInfo from './EventDetailedInfo'
+import EventDetailedChat from './EventDetailedChat'
+import EventDetailedSidebar from './EventDetailedSidebar'
 
 const EventDetailedPage = () => {
     return (
-        <div>
-            <h1>Event detailed page</h1>
-        </div>
+        <Grid>
+            <Grid.Column width={10}>
+                <EventDetailedHeader/>
+                <EventDetailedInfo/>
+                <EventDetailedChat/>
+
+            </Grid.Column>
+            <GridColumn width={6}>
+                <EventDetailedSidebar/>
+            </GridColumn>
+            </Grid>
     )
 }
 
---
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const EventDetailedSidebar = () => {
+    return (
+        <div>
+            <h1>Side bar</h1>
+        </div>
+    )
+}
+export default EventDetailedSidebar;
